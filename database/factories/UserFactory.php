5f97c44ce471596dcb754814323595e427fc36d5
Mohammed <mohammedemran@gmail.com>
2025-10-05 18:18:36 +0300

    Fix UserFactory: replace fake() with $this->faker for Laravel Cloud compatibility

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories.Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
@@ -24,8 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
